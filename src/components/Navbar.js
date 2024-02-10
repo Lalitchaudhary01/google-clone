7cb41fd6c1c05c1e49493d7a8eec3a2309a1bbb4
Lalitchdhary01 <Lalitkumar46789@gmail.com>
2024-02-10 12:26:37 +0530

    build navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Search from "./Search";
 
-const Navbar = () => {
-  return (
-    <div>
-      <h1>Navbar</h1>
+const Navbar = ({ darkTheme, setDarkTheme }) => (
+  <div className="nav-container">
+    <div className="nav-bar">
+      <Link to="/">
+        <p className="logo">Goggle 🔎</p>
+      </Link>
+      <button
+        type="button"
+        onClick={() => setDarkTheme(!darkTheme)}
+        className="dark-btn"
+      >
+        {darkTheme ? "💡 Light" : "🌙 Dark"}
+      </button>
+      <Search />
     </div>
-  );
-};
+  </div>
+);
 
 export default Navbar;
